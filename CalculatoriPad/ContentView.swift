3af2d230c913804c87075e8a20f9408c673cc2e7
daniel <daniel.d4@icloud.com>
2019-11-15 21:07:51 +0100

    Initial Commit

---
+++ CalculatoriPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatoriPad
+//
+//  Created by daniel d4 on 15/11/2019.
+//  Copyright © 2019 daniel d4. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
